9d17fecda7df5dc095ed0ea7d61115287bf2b289
sean dougherty <sd@kexp.org>
2024-11-22 08:55:19 -1000

    refactored tests: - added test for an empty supplied variable - created assertion function to handle got == want with a helper function

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const EnglishPrefix = "Hello, "
+
 func Hello(s string) string {
-	return "Hello, " + s
+	if s == "" {
+		return EnglishPrefix + "world"
+	}
+	return EnglishPrefix + s
 }
 func main() {
 	fmt.Println(Hello("you"))
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -1,12 +1,32 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
+// TestHello tests that saying hello to someone (supplied string) or to nobody (empty string supplied) works.
 func TestHello(t *testing.T) {
-	got := Hello("to you")
-	want := "Hello, to you"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("to you")
+		want := "Hello, to you"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+// assertCorrectMessage takes in t from the Test functions, as well as the got/want, and does the asserting
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
 }
